9ae1730f9d5ab3a7fd2a2d6609be48c52762ef86
trunovandrey21 <97969331+trunovandrey21@users.noreply.github.com>
2022-06-11 21:25:34 +0200

    Add files via upload

---
+++ DOM/script/script.js
@@ -0,0 +1,49 @@
+const products = [
+    {
+      name: "Iphone 12",
+      brand: "Apple",
+      price: 3200000,
+      properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
+    },
+    {
+      name: "Galaxy S20 Ultra",
+      brand: "Samsung",
+      price: 2900000,
+      properties: ["120 hz screen", "Water resistance"],
+    },
+    {
+      name: "MI 9",
+      brand: "Xiaomi",
+      price: 1300000,
+      properties: ["Best price", "Pay less - get more!"],
+    },
+  ];
+
+
+const renderProducts = (productsList) => {
+    const container = document.createElement('div');
+
+    productsList.forEach((prod) => {
+        const prodContainer = document.createElement('div');
+        const prodTitle = document.createElement('h3');
+        const prodSubtitle = document.createElement('h4');
+        const prodPropertiesList = document.createElement('ul');
+
+
+        prod.properties.forEach((prop) => {
+            const listItem = document.createElement('li');
+            listItem.innerText = prop;
+            prodPropertiesList.append(listItem);
+
+        })
+
+        prodTitle.innerText = prod.name;
+        prodSubtitle.innerText = prod.brand;
+
+        prodContainer.append(prodTitle, prodSubtitle, prodPropertiesList);
+        container.append(prodContainer);
+    });
+
+};
+
+renderProducts(products);
